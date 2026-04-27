afc06c5f20d8bec8fa9d64d566fc395fdc1d08da
maksumiykroman-code <maksumiykroman@gmail.com>
2026-04-27 12:23:02 +0300

    Update page.tsx

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,15 +1,14 @@
-import { getProductBySlug } from "@/lib/products";
+import { getProductBySlug } from "@/lib/shop";
 import { notFound } from "next/navigation";
 import ProductPageContent from "@/components/products/product-page-content";
 
-interface PageProps {
+export default async function ProductPage({
+  params,
+}: {
   params: Promise<{ slug: string }>;
-}
-
-export default async function ProductPage({ params }: PageProps) {
-  // Очікуємо (await) параметри, як того вимагає Next.js 15
+}) {
   const { slug } = await params;
-  const product = getProductBySlug(slug);
+  const product = await getProductBySlug(slug);
 
   if (!product) {
     notFound();
